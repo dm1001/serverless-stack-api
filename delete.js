7bb48a8736a78840b30431321552f73c4304abc7
Dusan M <dusan@musth.io>
2021-01-28 14:26:40 -0500

    Adding our Serverless API

--- delete.js
+++ delete.js
@@ -3,16 +3,12 @@ import dynamoDb from "./libs/dynamodb-lib";
 export const main = handler(async (event, context) => {
     const params = {
         TableName: process.env.tableName,
-        // 'Key' defines the partition key and sort key of the item to be retrieved
+        // 'Key' defines the partition key and sort key of the item to be removed
         Key: {
             userid: "123", // The id of the author
             noteid: event.pathParameters.id, // The id of the note from the path
         },
     };
-    const result = await dynamoDb.get(params);
-    if (!result.Item) {
-        throw new Error("Item not found.");
-    }
-    // Return the retrieved item
-    return result.Item;
+    await dynamoDb.delete(params);
+    return { status: true };
 });
